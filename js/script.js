f971c311dbcc4d37d118397c47b3f2ec9d73b5fa
Vins 2106 <69247128+Vins2106@users.noreply.github.com>
2022-01-22 00:04:34 +0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const form = document.getElementById("form");
+
+form.addEventListener("submit", (e) => {
+	const username = document.getElementById("username")
+	const email = document.getElementById("email");
+	const contoh = document.getElementById("contoh");
+
+	Email.send({
+		Host : "smtp.gmail.com",
+	    To : 'vins25165@gmail.com',
+	    From : "zhopgang@gmail.com",
+	    Username: "zhopgang@gmail.com",
+	    Password: "cdbczbvudmvsjvin",
+	    Subject : `${username.value} request a certificate`,
+	    Body : `Nama : ${username.value} <br /> Email : ${email.value} <br /> Contoh : ${contoh.value} <br /> ${Date.now()}`
+	}).then(message => {
+		console.log(message)
+		alert("Terima kasih. request kamu sedang di validasi, harap tunggu email dari kami.")
+	});	
+
+	e.preventDefault()
+})
